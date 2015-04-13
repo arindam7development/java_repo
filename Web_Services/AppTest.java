47a1deda4d4ee660ad49facb7181d736dd28f3a9
Arindam Mishra <arindam5191@gmail.com>
2015-04-13 12:40:14 +0530

    Update AppTest.java

--- Web_Services/AppTest.java
+++ Web_Services/AppTest.java
@@ -5,15 +5,15 @@ public class AppTest{
 	private int counter;
  
 	public void print(){
-		System.out.println("printIt() no param");
+		System.out.println("print() no param");
 	}
  
 	public void printString(String temp){
-		System.out.println("printIt() with param String : " + temp);
+		System.out.println("printString() with param String : " + temp);
 	}
  
 	public void printInt(int temp){
-		System.out.println("printIt() with param int : " + temp);
+		System.out.println("printInt() with param int : " + temp);
 	}
  
 	public void setCounter(int counter){
