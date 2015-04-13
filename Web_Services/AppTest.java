0295947499fde075f2e06f0b9dd49b3f02cf3b5a
Arindam Mishra <arindam5191@gmail.com>
2015-04-13 12:29:29 +0530

    Create AppTest.java
    This Class uses Reflection to call java method of the class ReflectApp.java at runtime .

---
+++ Web_Services/AppTest.java
@@ -0,0 +1,49 @@
+package org.arindam.reflection;
+
+import java.lang.reflect.Method;
+// This Class uses Reflection to call java method of the class ReflectApp.java at runtime .
+public class ReflectApp{
+ 
+	public static void main(String[] args) {
+ 
+	//no paramater
+	Class noparams[] = {};
+ 
+	//String parameter
+	Class[] paramString = new Class[1];	
+	paramString[0] = String.class;
+ 
+	//int parameter
+	Class[] paramInt = new Class[1];	
+	paramInt[0] = Integer.TYPE;
+ 
+	try{
+	        //load the AppTest at runtime
+		Class cls = Class.forName("org.arindam.reflection.AppTest");
+		Object obj = cls.newInstance();
+ 
+		//call the printIt method
+		Method method = cls.getDeclaredMethod("print", noparams);
+		method.invoke(obj, null);
+ 
+		//call the printItString method, pass a String param 
+		method = cls.getDeclaredMethod("printString", paramString);
+		method.invoke(obj, new String("arindam"));
+ 
+		//call the printItInt method, pass a int param
+		method = cls.getDeclaredMethod("printInt", paramInt);
+		method.invoke(obj, 1230);
+ 
+		//call the setCounter method, pass a int param
+		method = cls.getDeclaredMethod("setCounter", paramInt);
+		method.invoke(obj, 777);
+ 
+		//call the printCounter method
+		method = cls.getDeclaredMethod("printCounter", noparams);
+		method.invoke(obj, null);
+ 
+	}catch(Exception ex){
+		ex.printStackTrace();
+	}
+   }
+}
