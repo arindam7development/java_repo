6d9eb58927c09d1a3fcab43b5c7f180d6045703b
Arindam Mishra <arindam5191@gmail.com>
2017-01-01 00:12:18 +0530

    Update ReflectApp.java

--- Web_Services/ReflectApp.java
+++ Web_Services/ReflectApp.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Method;
 // This Class uses Reflection to call java method of the class AppTest.java at runtime .
 public class ReflectApp{
  
-	public static void main(String[] args) {
+	public static void main(String[] args1) {
  
 	//no paramater
 	Class noparams[] = {};
